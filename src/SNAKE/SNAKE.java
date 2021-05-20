cd256b2e00da51612db336da8ed88e4b3898decd
CelinProgramador <marcelo.filho@alunos.ifsuldeminas.edu.br>
2021-05-19 23:39:41 -0300

    V1.1
    - Velocidade alterada;
    - Pontuação mudada (1 para 10);

---
+++ src/SNAKE/SNAKE.java
@@ -0,0 +1,36 @@
+package SNAKE;
+
+// Importações necessárias para rodar o jogo
+import javax.swing.JFrame;
+
+public class SNAKE extends JFrame
+{
+
+    // Método construtor da classe
+    public SNAKE ()
+    {
+        // Adiciona o JPanel do jogo
+        add(new Grade());
+
+        // Define a saida da aplicação
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Define o tamanho da janela
+        setSize(420, 440);
+        // A localização
+        setLocationRelativeTo(null);
+        // O titulo da janela
+        setTitle("Jogo da Cobrinha");
+
+        // Impede o redimensionamento da janela
+        setResizable(false);
+        // Mostra a janela
+        setVisible(true);
+    }
+
+    public static void main(String[] args) 
+    {
+        // Cria o novo JFrame
+        new SNAKE();
+    }
+
+}
